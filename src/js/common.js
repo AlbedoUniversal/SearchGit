c13d0655b053eb358982ec55d7e73a79f80e0dbb
AlbedoUniversal <Albedogforce@gmail.com>
2019-12-20 17:05:53 +0300

    begin to work

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,22 @@
 const userCardHTML =
   '<img class="cards-item__photo" alt=""><p class="cards-item__login"></p><p class="cards-item__link"></p><p class="cards-item__rating"></p>';
-
-const div = createElement("div");
-
+const div = document.createElement("div");
 div.classList.add("cards-item");
 div.innerHtml = userCardHTML;
+
+let inputSearch = document.querySelector(".inputs-search"); //инпут, куда вводится текст поиска
+// let searchString = inputSearch.value; // сам текст поиска
+
+const btn = document.querySelector(".inputs-type"); //кнопка поиска
+
+function a(items) {
+  console.log(items);
+}
+
+btn.addEventListener("click", () => {
+  fetch(`https://api.github.com/search/users?q=${inputSearch.value}`)
+    .then(responce => responce.json())
+    .then(json => {
+      a(json.items);
+    });
+});
